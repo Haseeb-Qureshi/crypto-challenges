a3572b255a8576b6563081cda48f9d983403fc4a
Haseeb Qureshi <haseeb@haseebq.com>
2016-04-07 15:23:28 -0700

    Finish challenge 5

--- set1/c5.rb
+++ set1/c5.rb
@@ -1,12 +1,13 @@
-def challenge_5
-end
+require_relative '../ciphers/simple_xor/cipher'
 
-# Burning 'em, if you ain't quick and nimble
-# I go crazy when I hear a cymbal
-#
-# encrypt with ICE
-#
-# =>
-#
-# 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-# a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+def challenge_5
+  message = "Burning 'em, if you ain't quick and nimble\n"\
+            "I go crazy when I hear a cymbal"
+  key = "ICE"
+  cipher = XORCipher.new(message, key)
+
+  should_be_ciphertext = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+  cipher.encrypt!
+  cipher.to_hex == should_be_ciphertext
+end
--- tests.rb
+++ tests.rb
@@ -1,3 +1,4 @@
+SLOW_TESTS = [4]
 def test_output(i)
   output = colorize_output(send("challenge_#{i}"))
   puts "Challenge #{i}: #{output}"
@@ -8,5 +9,5 @@ def colorize_output(passed)
 end
 
 def run_tests(n)
-  (1..n).each { |num| test_output(num) }
+  (1..n).each { |num| test_output(num) unless SLOW_TESTS.include?(num) }
 end
